6dbc33e94261110a7539ad39a534290ec631d483
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-02-04 09:36:49 +0530

    Add files via upload

---
+++ forLoopSkipIntialixzation.java
@@ -0,0 +1,28 @@
+package java_eclipse;
+
+public class forLoopSkipIntialixzation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//skiping intialization
+	//	int i=1;
+//		for(;i<=10;i++) {
+//			System.out.println(i);
+//			
+//		}
+		//skiping condtion
+//		for(;;i++) {
+//			if(i>5) {
+//				break;
+//			}
+//			System.out.println("value of"+i); 
+//		}
+		//skiping iteration
+		for(int i=0;i<6;) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
---
+++ multipleforloop.java
@@ -0,0 +1,10 @@
+package java_eclipse;
+
+public class multipleforloop {
+	public static void main(String args[]) {
+		for(int i=1,j=10;i<=5&&i>=6;i++,j--) {
+			System.out.println(i+" "+j);
+		}
+	}
+
+}
