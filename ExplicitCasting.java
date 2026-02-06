fd83d68dc1d8cd0ae0f225ab11828cf0ae32faa7
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-02-06 10:03:44 +0530

    Add files via upload

---
+++ ExplicitCasting.java
@@ -0,0 +1,14 @@
+package java_eclipse;
+
+public class ExplicitCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num=59.89;
+		short num1=(short) num;
+		System.out.println(num);
+		System.out.println(num1);
+
+	}
+
+}
---
+++ jj.java
@@ -0,0 +1,11 @@
+package java_eclipse;
+
+public class jj {
+	public static void main(String args[]) {
+		int num=50;
+		double num1=num;
+		System.out.println(num);
+		System.out.println(num1);
+	}
+
+}
