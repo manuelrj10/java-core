797c91fb1ba9dfe9e47f2cf1d194dc1abb6ebc47
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-02-27 10:49:57 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,19 @@
+class palindrome{
+    public static void main(String[] args){
+        int num=121;
+        int temp=num;
+        int rev=0;
+        while(num>0){
+            int rem=num%10;
+             rev=rev*10+rem;
+            num=num/10;
+        }
+        System.out.println(rev);
+        if(temp==rev){
+            System.out.println("palindrtome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+    }
+}
